43afd5ff3631d69cc040865189a7d9692d92eda4
Rana Salem <ranatasalem@gmail.com>
2025-02-14 00:03:16 +0000

    wip

--- app/mongo/connect.js
+++ app/mongo/connect.js
@@ -7,7 +7,7 @@ const connectToMongoDB = async () => {
   const db = mongoClient.db(mongoConfig.get('databaseName'))
   const collection = db.collection(mongoConfig.get('emailCollection'))
 
-  const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+  const insertResult = await collection.insertMany([{ a: 4 }, { a: 5 }, { a: 6 }])
   console.log('Inserted documents =>', insertResult)
 
   const findResult = await collection.find({}).toArray()
